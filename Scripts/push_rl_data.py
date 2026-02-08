db57df3437bc260b71d917e37024d19e7e8acd40
Astune <lys.2026485907@gmail.com>
2026-02-09 10:44:02 +1100

    fix push_rl_data.py

--- Scripts/push_rl_data.py
+++ Scripts/push_rl_data.py
@@ -1,7 +1,9 @@
 import logging
+import os
 
 import torch
-from huggingface_hub import login
+from datasets import load_dataset
+from huggingface_hub import login, upload_file
 
 from data.loader import load_piast_dataset
 from model.mts_config import MTSGenConfig
@@ -17,24 +19,37 @@ continue_on_exception = 1
 linux = 0
 
 if __name__ == "__main__":
-    dataset = load_piast_dataset(repo_path="data/dataset/PIAST", download_if_empty=True) if linux \
+    piast = load_piast_dataset(repo_path="data/dataset/PIAST", download_if_empty=True) if linux \
         else load_piast_dataset(repo_path="../data/dataset/PIAST", download_if_empty=True)
 
-    dataset = MIDISegmentDataset(
-        midi_dataset=dataset["piast-yt"],
-        batch=16,
-        length_seconds=8,
-        sample_rate=24000,
-        overlap_ratio=0.3,
-        shuffle=False,
-        infinite=False,
-        min_notes_per_segment=3,
-        random_start=False
-    )
+    size = len(piast["piast-yt"])
+    segment = 10
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_dir = os.path.dirname(current_dir)
 
-    dataset = create_hf_dataset_from_midi_segment(dataset)
+    for idx in range(size // segment):
 
-    login("")
+        dataset = MIDISegmentDataset(
+            midi_dataset=piast["piast-yt"].select(range(idx * segment, (idx + 1) * segment)),
+            batch=16,
+            length_seconds=8,
+            sample_rate=24000,
+            overlap_ratio=0.3,
+            shuffle=False,
+            infinite=False,
+            min_notes_per_segment=3,
+            random_start=False
+        )
 
-    dataset.push_to_hub("astune/mts_rl_dataset")
+        dataset = create_hf_dataset_from_midi_segment(dataset)
+
+        dataset.to_parquet(os.path.join(current_dir, "/.ache/dataset.parquet"))
+
+        upload_file(
+            path_or_fileobj=os.path.join(current_dir, "/.ache/dataset.parquet"),
+            path_in_repo=f"data/batch_{idx}.parquet",  # 指定存储路径和文件名
+            repo_id="astune/mts_rl_dataset",
+            repo_type="dataset",
+            token=""  # 需要写权限
+        )
 
